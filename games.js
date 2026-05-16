f54bd3b28316d14db6b954b922412d289eceb2fb
Nail120212 <ianangeloi122@gmail.com>
2026-05-16 21:07:26 +0800

    Update games.js

--- games.js
+++ games.js
@@ -7,8 +7,7 @@ const GAMES = [];
   gamelogo = "https://tr.rbxcdn.com/180DAY-a79b1f2af4319127d5a9e765c3b58d29/768/432/Image/Webp/noFilter";
   status = "Undetected";
   changelogs = [
-    { date: "2026-05-16", text: "Initial release. Core features added." },
-    { date: "2026-05-10", text: "Beta testing phase started." },
+    { date: "2026-05-16", text: "script release. Main features added." },
   ];
   GAMES.push({ name: gamename, logo: gamelogo, status: status, changelogs: changelogs });
 
