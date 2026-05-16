74810a60bbac651f87bbc218a8b6724aaac19005
Nail120212 <ianangeloi122@gmail.com>
2026-05-16 14:56:48 +0800

    Add files via upload

---
+++ games.js
@@ -0,0 +1,9 @@
+const GAMES = [];
+
+(function () {
+  let gamename, gamelogo;
+
+  gamename = "Diesel n Steel";
+  gamelogo = "https://tr.rbxcdn.com/180DAY-a79b1f2af4319127d5a9e765c3b58d29/768/432/Image/Webp/noFilter";
+  GAMES.push({ name: gamename, logo: gamelogo });
+})();
